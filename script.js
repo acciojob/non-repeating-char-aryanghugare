b340d6c47791198543db415fb20560183828ab89
Aryan Ghugare <aryanghugare75@gmail.com>
2025-12-06 02:55:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
-const input = prompt("Enter a string");
+	let f = false ;
+for(let i = 1 ; i < str.length-1 ; i++){
+	if(str[i-1]!=str[i] && str[i+1]!=str[i]) {
+		return str[i];
+	}
+}	
+
+	if(!f) return null ;
+} 
+ const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
