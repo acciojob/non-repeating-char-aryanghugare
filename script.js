06ad243fb38504b49c0690205942a505b9f2cfa0
Aryan Ghugare <aryanghugare75@gmail.com>
2025-12-06 03:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	let f = false ;
-for(let i = 0 ; i < str.length ; i++){
-	if(i==0 && str.length>1) {
-		if(str[0]!=str[1]) return str[0];
+	for(let i = 0 ; i < str.length ; i++){
+		f = false ;
+		let s = str[i]
+		for(let j = 0 ; j < str.length ; j++){
+			if(j!=i && str[j]==s) f = true ;
+		}
+		if(!f) return str[i];  
 	}
-	if(i==str.length-1){
-		if(str[i]!=str[i-1]) return str[i];
-	}
-	if( (i>0 && str[i-1]!=str[i])  && (i<str.length-1 && str[i+1]!=str[i])) {
-		return str[i];  
-	}  
-}	
 	
- 
-	if(!f) return null ;
+return null ;
 }  
  const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
